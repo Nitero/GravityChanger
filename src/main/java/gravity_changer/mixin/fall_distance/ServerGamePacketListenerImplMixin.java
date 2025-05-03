6be57cdaf807add4cae62cc980a8bc3dbcc99bf6
Nito <nikolas1997@hotmail.de>
2025-05-03 12:31:28 +0200

    better fix for fall damage with gravity up

--- src/main/java/gravity_changer/mixin/fall_distance/ServerGamePacketListenerImplMixin.java
+++ src/main/java/gravity_changer/mixin/fall_distance/ServerGamePacketListenerImplMixin.java
@@ -1,27 +1,40 @@
 package gravity_changer.mixin.fall_distance;
 
 import gravity_changer.api.GravityChangerAPI;
+import gravity_changer.util.RotationUtil;
 import net.minecraft.core.Direction;
+import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(ServerGamePacketListenerImpl.class)
 public abstract class ServerGamePacketListenerImplMixin {
 
-	@Redirect(
+	@Shadow
+	public ServerPlayer player;
+
+	@Shadow
+	private double lastGoodX, lastGoodY, lastGoodZ;
+
+	@ModifyVariable(
 			method = "handleMovePlayer",
-			at = @At(
-					value = "INVOKE",
-					target = "Lnet/minecraft/server/level/ServerPlayer;resetFallDistance()V"
-			)
+			at = @At(value = "STORE", ordinal = 0),
+			ordinal = 0,
+			name = "flag"
 	)
-	private void redirect_resetFallDistance(ServerPlayer player) {
+	private boolean modifyFlagBasedOnGravity(boolean originalFlag, ServerboundMovePlayerPacket packet) {
 		Direction gravity = GravityChangerAPI.getGravityDirection(player);
 
-		if (gravity == Direction.DOWN)
-			player.resetFallDistance();
+		double dx = packet.getX(player.getX()) - lastGoodX;
+		double dy = packet.getY(player.getY()) - lastGoodY;
+		double dz = packet.getZ(player.getZ()) - lastGoodZ;
+
+		Vec3 localVec = RotationUtil.vecWorldToPlayer(dx, dy, dz, gravity);
+		return localVec.y > 0.0;
 	}
 }
